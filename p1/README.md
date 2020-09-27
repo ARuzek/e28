bfc6545a7864cc0ee079ca8b7b68a901c910750d
Amelia Ruzek <amelia.ruzek.klein@gmail.com>
2020-09-26 21:32:55 -0500

    get started with some vue set up

--- p1/README.md
+++ p1/README.md
@@ -1,1 +1,4 @@
-# About this game
+# Resources I used
+
+https://vuejs.org/v2/guide/#Conditionals-and-Loops
+https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval
--- p1/script.js
+++ p1/script.js
@@ -2,7 +2,16 @@ let app = new Vue({
   el: "#app",
   data: {
     clock: 0,
-    hauntedHappenings: null,
-    hauntedRooms: [],
+    hauntedHappenings: "something happened",
+    house: [
+      { name: "The Observatory" },
+      { name: "The Library" },
+      { name: "The Basement" },
+    ],
+  },
+  methods: {
+    getTime: function () {
+      window.setInterval(this.clock++, 1000);
+    },
   },
 });
