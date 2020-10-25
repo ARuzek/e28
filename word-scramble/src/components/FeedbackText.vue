a4c2c481a2b11f3a1c4f35648bc0332ee29ed47f
Amelia Ruzek <amelia.ruzek.klein@gmail.com>
2020-10-25 14:23:46 -0500

    make attempt - starting from week 6 answer

---
+++ word-scramble/src/components/FeedbackText.vue
@@ -0,0 +1,33 @@
+<template>
+<div v-if='feedback' class='feedback' :class="{ pass : correct, fail: !correct }">
+     <div v-if='correct'>You got it! Nice work. <button @click='loadGame'>Play again</button></div>
+     <div v-else>Sorry, that’s not correct. Please try again.</div>
+</div>
+</template>
+
+<script>
+
+export default {
+  props: ["feedback", "correct"],
+}
+
+</script>
+
+ <style>
+ 
+        .pass {
+            background-color: green;
+        }
+
+        .fail {
+            background-color: red;
+        }
+
+        .feedback {
+            display: inline-block;
+            padding: 5px;
+            margin: 10px 0;
+            color: white;
+            font-weight: bold;
+        }
+   </style>     
