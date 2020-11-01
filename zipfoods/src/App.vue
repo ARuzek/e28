0cbe75c8b9212ea8691175fb04da952abfd567c2
Amelia Ruzek <amelia.ruzek.klein@gmail.com>
2020-11-01 10:25:30 -0600

    learn routing

--- zipfoods/src/App.vue
+++ zipfoods/src/App.vue
@@ -3,14 +3,19 @@
     <img alt="ZipFoods logo" id="logo" src="@/assets/images/zipfoods-logo.png">
 <nav>
     <ul>
-        <li v-for='link in links' :key='link'>
-            <a href='#' @click='page = link'>{{ link }}</a>
+        <li>
+            <router-link
+                v-for="link in links"
+                v-bind:key="link"
+                v-bind:to="paths[link]"
+                exact
+                >{{ link }}</router-link
+            >
         </li>
     </ul>
 </nav>
 
-<component v-bind:is='linkComponents[page]'></component>
-
+<router-view></router-view>
 
     
 
@@ -19,31 +24,21 @@
 
 <script>
  
- import HomePage from './components/pages/HomePage.vue'
- import ProductsPage from './components/pages/ProductsPage.vue'
- import ShowCategories from './components/pages/ShowCategories.vue'
-
+ 
 export default {
   name: 'App',
-  components: {
-   HomePage, 
-   ProductsPage, 
-   ShowCategories
-   },
+ 
   data() {
     return{
-      /* Default page to start with */
-    page: 'home',
+  /* Store links in an array to maintain order */
+links: ['home', 'products', 'categories'],
 
-    /* Store links in an array to maintain order */
-    links: ['home', 'products', 'categories'],
-
-    /* Map links to the appropriate component */
-    linkComponents: {
-    home: 'home-page',
-    products: 'products-page',
-    categories: 'show-categories'
-}
+/* Map links to the appropriate component */
+paths: {
+    home: '/',
+    products: '/products',
+    categories: '/categories',
+},
     }
   } 
 }
--- zipfoods/src/main.js
+++ zipfoods/src/main.js
@@ -1,8 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import VueRouter from "vue-router";
+import HomePage from "./components/pages/HomePage.vue";
+import ProductsPage from "./components/pages/ProductsPage.vue";
+import ShowCategories from "./components/pages/ShowCategories.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: history,
+  routes: [
+    { path: "/", component: HomePage },
+    { path: "/products", component: ProductsPage },
+    { path: "/categories", component: ShowCategories },
+  ],
+});
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
