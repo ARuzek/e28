7ac8e629bfce55ca2fb3f86e1f83c5786ff28bbe
Amelia Ruzek <amelia.ruzek.klein@gmail.com>
2020-11-10 17:39:52 -0600

    correct typo

--- p2/src/components/pages/Post.vue
+++ p2/src/components/pages/Post.vue
@@ -23,7 +23,7 @@ export default {
     name: '',
     props: ['id', 'posts'],
     components: {
-        'show-post': Showpost,
+        'show-post': ShowPost,
     },
     data() {
         return {};
