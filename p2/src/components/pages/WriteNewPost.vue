abed758ec883ecba181bb4d636cb6c9dbe721576
Amelia Ruzek <amelia.ruzek.klein@gmail.com>
2020-11-16 18:36:57 -0600

    Style it

--- p2/src/components/pages/WriteNewPost.vue
+++ p2/src/components/pages/WriteNewPost.vue
@@ -3,14 +3,14 @@
         <h2>Add More Stories About Your Cats</h2>
 
         <div id="inputs">
-            <label for="title">Add Title Here</label>
-            <input type="text" v-model="post.title" id="title" />
+            <label for="title">Add Your Blog Post Title Here:
+            <input type="text" v-model="post.title" id="title" /></label>
 <!--Add a real drag and drop with POST back to the server if you have time-->
-            <label for="image">Add an image</label>
-            <input type="text" v-model="post.image" id="image"/>
+            <label for="image">Add An Image Name:
+            <input type="text" v-model="post.image" id="image"/></label>
 
-            <label for="content">Write Your Blog Post</label>
-            <textarea v-model="post.content" id="content"></textarea>
+            <label for="content">Write Your Blog Post Here:</label>
+            <textarea v-model="post.content" id="content" rows="50" cols="116"></textarea>
         </div>
         <button @click="addPost">Create New Blog Post</button>
         <p v-if="!showConfirmationMessage">
@@ -28,11 +28,9 @@ export default {
             errors: null,
             showConfirmationMessage: false,
             post: {
-                title: 'Add Title Here',
-                image: 'Add image',
-                content:
-                    'Write Blog Here',
-                
+                title: '',
+                image: '',
+                content: '', 
             },
         };
     },
@@ -56,5 +54,34 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+   h2, p, label, button{
+    text-align: center;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+    div {
+    max-width: 900px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    }
+    input, button, textarea {
+    border-radius: 1rem;
+    border: 1px solid pink;
+    }
+    label {
+        margin: 1rem;
+        text-align: left;
+    }
+    button {
+        padding: 1rem; 
+        background-color: pink;
+        font-size: 1.2rem;
+        margin: 1rem;
+    }
+    button:hover {
+        background-color: lightpink;
+    }
+   
+    
 </style>
