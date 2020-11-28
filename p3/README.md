0f57e4654c4e35b0465d6c883ac435d369734561
Amelia Ruzek <amelia.ruzek.klein@gmail.com>
2020-11-28 10:10:23 -0600

    add drawing tool

--- p3/README.md
+++ p3/README.md
@@ -15,4 +15,6 @@ Font Citation: https://fonts.google.com/specimen/Pacifico#license
 
 Comic illustrations of my cats are self-drawn, lol, but at least I had some fun with it. :)
 
+Canvas drawing tool from http://www.williammalone.com/articles/create-html5-canvas-javascript-drawing-app/#demo-simple and refactored for Vue
+
 ## Notes for instructor
--- p3/src/components/pages/WriteNewPost.vue
+++ p3/src/components/pages/WriteNewPost.vue
@@ -5,9 +5,12 @@
         <div id="inputs">
             <label for="title">Add Your Blog Post Title Here:
             <input type="text" v-model="post.title" id="title" /></label>
+            <cite>Required. Must be 3 - 100 characters. </cite>
 <!--Add a real drag and drop with POST back to the server if you have time-->
             <label for="image">Add An Image Name:
             <input type="text" v-model="post.image" id="image"/></label>
+ 
+            <canvas width="200px" height="400px" id="canvas" style="border:1px solid"  @mousedown="startDrawingComic($event)"  @mousemove="drawHere($event)"  @mouseup="stopDrawingComic"></canvas>
 
             <label for="content">Write Your Blog Post Here:</label>
             <textarea v-model="post.content" id="content" rows="50" cols="116"></textarea>
@@ -32,9 +35,54 @@ export default {
                 image: '',
                 content: '', 
             },
+            paint: false,
+            clickX: [],
+            clickY: [],
+            clickDrag:[],
         };
     },
     methods: {
+        startDrawingComic(event){
+            console.log("Start drawing");                
+            this.paint = true;
+            this.addClick(event.pageX - event.target.offsetLeft, event.pageY - event.target.offsetTop);
+            this.redraw(event.target);
+        },
+        drawHere(event){
+            console.log("Draw Here");
+            if(this.paint){
+            this.addClick(event.pageX - event.target.offsetLeft, event.pageY - event.target.offsetTop, true);
+            this.redraw(event.target);
+        }
+        },
+        stopDrawingComic(){
+            console.log("Stop drawing");
+            this.paint = false;
+        },
+        redraw(context){
+            context = context.getContext("2d");
+            context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+            context.strokeStyle = "#df4b26";
+            context.lineJoin = "round";
+            context.lineWidth = 5;
+                        
+            for(var i=0; i < this.clickX.length; i++) {		
+                context.beginPath();
+                if(this.clickDrag[i] && i){
+                context.moveTo(this.clickX[i-1], this.clickY[i-1]);
+                }else{
+                context.moveTo(this.clickX[i]-1, this.clickY[i]);
+                }
+                context.lineTo(this.clickX[i], this.clickY[i]);
+                context.closePath();
+                context.stroke();
+            }
+        },
+        addClick(x, y, dragging){
+            this.clickX.push(x);
+            this.clickY.push(y);
+            this.clickDrag.push(dragging);
+        },
         addPost() {
             axios.post('/post', this.post).then((response) => {
                 if (response.data.errors) {
@@ -51,6 +99,7 @@ export default {
             
         },
     },
+    
 };
 </script>
 
@@ -58,6 +107,14 @@ export default {
    h2, p, label, button{
     text-align: center;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+    h2 {
+            font-family: 'Pacifico', cursive;
+
+    }
+    canvas {
+        width: 200px; 
+        height: 400px; 
     }
     div {
     max-width: 900px;
@@ -82,6 +139,9 @@ export default {
     button:hover {
         background-color: lightpink;
     }
-   
+    cite {
+        margin-top: -10px; 
+        margin-left: 16px; 
+    }
     
 </style>
