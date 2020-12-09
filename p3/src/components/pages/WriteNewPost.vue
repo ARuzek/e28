8ca240d459b71b189179efe19dd24ef360de42ef
Amelia Ruzek <amelia.ruzek.klein@gmail.com>
2020-12-08 19:46:31 -0600

    update write new post page

--- p3/src/components/pages/WriteNewPost.vue
+++ p3/src/components/pages/WriteNewPost.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <div v-if="user">
         <h2>Add More Stories About Your Cats</h2>
 
         <div id="inputs">
@@ -20,6 +21,64 @@
         {{ errors }}
         </p>
         <p v-if="showConfirmationMessage">Congrats! Your post is now published!</p>
+        </div>
+        <div v-else>
+            <h2>Login</h2>
+            <div>
+                <label
+                    >Email:
+                    <input
+                        type="text"
+                        data-test="email-input"
+                        v-model="data.email"
+                /></label>
+            </div>
+            <div>
+                <label
+                    >Password:
+                    <input
+                        type="password"
+                        data-test="password-input"
+                        v-model="data.password"
+                /></label>
+            </div>
+
+            <button @click="login" data-test="login-button">Login</button>
+
+            <ul v-if="errors">
+                <li class="error" v-for="(error, index) in errors" :key="index">
+                    {{ error }}
+                </li>
+            </ul>
+
+            <h2> Or Register </h2>
+            <label
+                    >Name Please:
+                    <input
+                        type="text"
+                        data-test="register-name-input"
+                        v-model="registration.name"
+                /></label>
+                <label
+                    >Set an Email:
+                    <input
+                        type="text"
+                        data-test="register-email-input"
+                        v-model="registration.email"
+                /></label>
+            </div>
+            <div>
+                <label
+                    >Set Your Password:
+                    <input
+                        type="password"
+                        data-test="register-password-input"
+                        v-model="registration.password"
+                /></label>
+            </div>
+            <button @click="register" data-test="register-button">Register</button>
+    
+        </div>
     </div>
 </template>
 
@@ -28,6 +87,15 @@ import { axios } from '@/common/app.js';
 export default {
     data() {
         return {
+            data: {
+                email: '',
+                password: '',
+            },
+            registration: {
+                name: '',
+                email: '',
+                password: '',
+            },
             errors: null,
             showConfirmationMessage: false,
             post: {
@@ -41,7 +109,39 @@ export default {
             clickDrag:[],
         };
     },
+    computed: {
+        // Get our user state from the Vuex store
+        user() {
+            return this.$store.state.user;
+        },
+    },
     methods: {
+        login() {
+            axios.post('login', this.data).then((response) => {
+                if (response.data.authenticated) {
+                    this.$store.commit('setUser', response.data.user);
+                } else {
+                    this.errors = response.data.errors;
+                }
+            });
+        },
+        register() {
+            axios.post('register', this.registration).then((response) => {
+               if (response.data.errors) {
+                    this.errors = response.data.errors;
+                }  else {
+                    this.$store.dispatch('authUser');
+                } 
+              
+            });
+        },
+        logout() {
+            axios.post('logout').then((response) => {
+                if (response.data.success) {
+                    this.$store.commit('setUser', null);
+                }
+            });
+        },
         startDrawingComic(event){
             console.log("Start drawing");                
             this.paint = true;
